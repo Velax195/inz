229f563ea4498f7001ca4cecb66cc25910e438cc
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-11-20 18:40:44 +0100

    changed network requests to Volley

--- app/src/main/java/com/kszych/pms/ScanRFIDActivity.java
+++ app/src/main/java/com/kszych/pms/ScanRFIDActivity.java
@@ -7,87 +7,86 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.kszych.pms.utils.DatabaseHelper;
-import com.kszych.pms.utils.GetRFIDTask;
-import com.kszych.pms.utils.GetRFIDTaskCompleteListener;
 import com.kszych.pms.utils.Package;
 
-public class ScanRFIDActivity extends AppCompatActivity implements GetRFIDTaskCompleteListener {
+public class ScanRFIDActivity extends AppCompatActivity {
 
 
     public static final String DEFAULT_RESPOND_MESSAGE = "CARD_NOT_PRESENT";
     public static final String NOT_READABLE_RESPOND_MESSAGE = "CARD_NOT_READABLE";
 
-    String ip = "http://192.168.0.14/scaner";
+    private static final String REQUEST_TAG = "ScannerRequest";
+
+    String mRequestURL = "http://192.168.0.14/scaner";
     Package mPackage;
     DatabaseHelper mDb = DatabaseHelper.getInstance(this);
-
-    private GetRFIDTask mGetRFIDTask;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_rfid);
 
-        //TODO different actions from different activities
-//
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
-
-
-//        mPackage = mDb.getPackageFromID(UidDec);
-//        if (previousActivity == getResources().getString(R.string.menuActivityName)) {
-//            Intent sIntent = new Intent(ScanRFIDActivity.this, PackageSingleActivity.class);
-//            sIntent.putExtra("SCANNED_PACKAGE", mPackage);
-//            startActivity(sIntent);
-//        } else if (previousActivity == getResources().getString(R.string.packageListActivityName)) {
-//            if (mDb.isInDatabase(UidDec) == true) {
-//                //TODO dialog
-//                AlertDialog.Builder builder = new AlertDialog.Builder(ScanRFIDActivity.this);
-//                builder.setTitle(R.string.warning)
-//                        .setMessage(R.string.overwritePackage)
-//                        .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                //TODO delete package, go to empty ModifyPackageActivity
-//                                Toast.makeText(ScanRFIDActivity.this
-//                                        , "DELETED! Just kidding", Toast.LENGTH_SHORT).show();
-//                            }
-//                        })
-//                        .setNegativeButton(R.string.showInfo, new DialogInterface.OnClickListener() {
-//                            @Override
-//                            public void onClick(DialogInterface dialogInterface, int i) {
-//                                Intent sIntent = new Intent(ScanRFIDActivity.this, PackageSingleActivity.class);
-//                                sIntent.putExtra("SCANNED_PACKAGE", mPackage);
-//                                startActivity(sIntent);
-//                            }
-//                        })
-//                        .show();
-//            } else {
-//                Intent sIntent = new Intent(ScanRFIDActivity.this, ModifyPackageActivity.class);
-//                sIntent.putExtra("SCANNED_PACKAGE", mPackage);
-//                startActivity(sIntent);
-//            }
-//        }
-//        doneFlag = false;
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        mGetRFIDTask = new GetRFIDTask(ip, ScanRFIDActivity.this);
-        mGetRFIDTask.execute();
+        sendRequest();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mGetRFIDTask.cancel(true);
+        if(mRequestQueue != null) {
+            mRequestQueue.cancelAll(REQUEST_TAG);
+        }
     }
 
-    @Override
-    public void cardIsRead(String uidHex, String uidDec) {
+    private void sendRequest() {
+        StringRequest request = new StringRequest(Request.Method.GET, mRequestURL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(isDefaultMessage(response)) {
+                    sendRequest();
+                }
+                else {
+                    String[] uids = response.split("\n");
+                    cardIsRead(uids[0], uids[1]);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO ignore, resend?
+                sendRequest();
+            }
+        });
+        request.setShouldCache(false).setTag(REQUEST_TAG);
+        mRequestQueue.add(request);
+    }
+
+    public boolean isDefaultMessage(String response) {
+        if (response.equals(ScanRFIDActivity.DEFAULT_RESPOND_MESSAGE)) {
+            // loop TODO delete this if
+        } else if (response.equals(ScanRFIDActivity.NOT_READABLE_RESPOND_MESSAGE)) {
+            // TODO toast here - card not compatible
+        } else {
+            // stop
+            return false;
+        }
+        return true;
+    }
+
+    private void cardIsRead(String uidHex, String uidDec) {
         Toast.makeText(ScanRFIDActivity.this, "Read " + uidHex + " " + uidDec, Toast.LENGTH_SHORT).show();
         onBackPressed();
         mPackage = mDb.getPackageByRFID(uidDec);
--- app/src/main/java/com/kszych/pms/utils/GetRFIDTaskCompleteListener.java
+++
@@ -1,5 +0,0 @@
-package com.kszych.pms.utils;
-
-public interface GetRFIDTaskCompleteListener {
-    void cardIsRead(String uidHex, String uidDec);
-}
