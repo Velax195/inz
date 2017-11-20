1e3870bb1ea027912a126942ec2163effab8aee2
Krzysztof Szych <222852@student.pwr.edu.pl>
2017-11-20 19:25:50 +0100

    added quick implementation of displaying packages containing part

--- app/src/main/java/com/kszych/pms/PartSingleActivity.java
+++ app/src/main/java/com/kszych/pms/PartSingleActivity.java
@@ -1,7 +1,16 @@
 package com.kszych.pms;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kszych.pms.utils.DatabaseHelper;
@@ -31,11 +40,41 @@ public class PartSingleActivity extends AppCompatActivity {
                 // TODO die panic etc
             }
             mPackagesOfPart = mDb.getPackagesContainingPart(mPart);
+
+            ListView packagesList = findViewById(R.id.singlePartPackagesList);
+            PackagesInPartArrayAdapter adapter = new PackagesInPartArrayAdapter(this, mPackagesOfPart);
+            packagesList.setAdapter(adapter);
+
         }
         else {
             // TODO die etc. We require extras
             Toast.makeText(this, "PART IS NULLL WHAAAT TO DOOOOO", Toast.LENGTH_SHORT).show();
             onBackPressed();
+        }
+    }
+
+    class PackagesInPartArrayAdapter extends ArrayAdapter<Package> {
+
+        private Context mContext;
+        private ArrayList<Package> mParts;
+
+        public PackagesInPartArrayAdapter(@NonNull Context context, ArrayList<Package> objects) {
+            super(context, android.R.layout.simple_list_item_1, objects);
+            mParts = objects;
+            mContext = context;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if(convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
+            }
+            TextView listItemText = convertView.findViewById(android.R.id.text1);
+            listItemText.setText(mParts.get(position).getRfidTag());
+
+            return convertView;
         }
     }
 }
